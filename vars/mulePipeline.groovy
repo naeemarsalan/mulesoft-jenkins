0dbec199d8a1e85f7c7ed80c61136d923fde0e71
Serghey Rodin <srodin@ms3-inc.com>
2020-03-20 22:14:38 +0200

    testing get_resource_dir method

--- vars/mulePipeline.groovy
+++ vars/mulePipeline.groovy
@@ -1,12 +1,26 @@
 // This pipeline requires no parameters as input
 
+import groovy.transform.SourceURI
+import java.nio.file.Path
+import java.nio.file.Paths
+
+class ScriptSourceUri {
+    @SourceURI
+    static URI uri
+}
+
+def call() {
+    Path scriptLocation = Paths.get(ScriptSourceUri.uri)
+    return scriptLocation.getParent().getParent().resolve('resources').toString()
+}
+
 def call(Map pipelineParams) {
   pipeline {
     agent any
     stages {
       stage('Test') {
         steps {
-          sh 'pwd'
+          sh "ls ${get_resource_dir()}"
         }
       }
     }
