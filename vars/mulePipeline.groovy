d42743323822f5d83fd4b7a291e2d323de69df26
Serghey Rodin <srodin@ms3-inc.com>
2020-03-20 22:45:26 +0200

    added maven test stage

--- vars/mulePipeline.groovy
+++ vars/mulePipeline.groovy
@@ -11,7 +11,27 @@ def call(Map pipelineParams) {
     stages {
       stage('Test') {
         steps {
-          sh 'echo ok'
+          container('maven') {
+            script {
+              configFileProvider([configFile(fileId: 'maven_settings', variable: 'MAVEN_SETTINGS_FILE')]) {
+                withCredentials([usernamePassword(credentialsId: 'devoptions', passwordVariable: 'appkey', usernameVariable: 'devenv')]) {
+                  if (Boolean.parseBoolean(env.env_override)) {
+                    sh "mvn -s '$MAVEN_SETTINGS_FILE' clean test -Denv=${env_var} -Dapp.key=${appkey}"
+                  } else {
+                    sh "mvn -s '$MAVEN_SETTINGS_FILE' clean test -Denv=${devenv} -Dapp.key=${appkey}"
+                  }
+                  publishHTML (target: [
+                    allowMissing: false,
+                    alwaysLinkToLastBuild: false,
+                    keepAll: true,
+                    reportDir: 'target/site/munit/coverage',
+                    reportFiles: 'summary.html',
+                    reportName: "Coverage Report"
+                  ])
+                }
+              }
+            }
+          }
         }
       }
     }
