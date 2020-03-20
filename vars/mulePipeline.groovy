214f82634997213bdc67858b05b91d16ab2efe86
Serghey Rodin <srodin@ms3-inc.com>
2020-03-20 22:33:13 +0200

    testing libraryResource

--- vars/mulePipeline.groovy
+++ vars/mulePipeline.groovy
@@ -1,26 +1,17 @@
 // This pipeline requires no parameters as input
 
-import groovy.transform.SourceURI
-import java.nio.file.Path
-import java.nio.file.Paths
-
-class ScriptSourceUri {
-    @SourceURI
-    static URI uri
-}
-
-def call() {
-    Path scriptLocation = Paths.get(ScriptSourceUri.uri)
-    return scriptLocation.getParent().getParent().resolve('resources').toString()
-}
 
 def call(Map pipelineParams) {
   pipeline {
-    agent any
+    agent {
+      kubernetes {
+        yaml libraryResource('kubernetes.yaml')
+      }
+    }
     stages {
       stage('Test') {
         steps {
-          sh "ls ${get_resource_dir()}"
+          sh 'echo ok'
         }
       }
     }
