29d1125defdf71bca6b09ae5c6cfee839e1bf00b
Serghey Rodin <srodin@ms3-inc.com>
2020-03-21 12:54:32 +0200

    added printenv to get branch name

--- vars/mulePipeline.groovy
+++ vars/mulePipeline.groovy
@@ -13,6 +13,8 @@ def call(Map pipelineParams) {
       stage('Test') {
         steps {
           container('maven') {
+            sh "echo ${BRANCH_NAME}"
+            sh 'printenv'
             script {
               configFileProvider([configFile(fileId: 'maven_settings', variable: 'MAVEN_SETTINGS_FILE')]) {
                   withCredentials([usernamePassword(credentialsId: 'devoptions', passwordVariable: 'appkey', usernameVariable: 'appenv')]) {
