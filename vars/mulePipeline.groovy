69426a034440f9814e844da126025056926bf5f7
Serghey Rodin <srodin@ms3-inc.com>
2020-03-26 21:18:44 +0200

    moved publishHTML block back inside withCredentials section

--- vars/mulePipeline.groovy
+++ vars/mulePipeline.groovy
@@ -35,15 +35,15 @@ def call(Map pipelineParams) {
               configFileProvider([configFile(fileId: 'maven_settings', variable: 'MAVEN_SETTINGS_FILE')]) {
                 withCredentials([usernamePassword(credentialsId: 'devoptions', passwordVariable: 'appkey', usernameVariable: 'appenv')]) {
                   sh "mvn -s '$MAVEN_SETTINGS_FILE' clean test -Denv=${maven_env} -Dapp.key=${appkey}"
+                  publishHTML (target: [
+                    allowMissing: false,
+                    alwaysLinkToLastBuild: false,
+                    keepAll: true,
+                    reportDir: 'target/site/munit/coverage',
+                    reportFiles: 'summary.html',
+                    reportName: "Coverage Report"
+                  ])
                 }
-                publishHTML (target: [
-                  allowMissing: false,
-                  alwaysLinkToLastBuild: false,
-                  keepAll: true,
-                  reportDir: 'target/site/munit/coverage',
-                  reportFiles: 'summary.html',
-                  reportName: "Coverage Report"
-                ])
               }
             }
           }
