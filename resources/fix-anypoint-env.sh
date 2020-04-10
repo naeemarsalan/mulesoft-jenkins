d7367bd5bd1588ee6663578121bfcf374e9b10c9
Fiodor Andruhin <fandruhin@ms3-inc.com>
2020-04-10 12:33:58 +0300

    fixed the bash script logic again (removed condition for yaml file) (DOPS-219)

--- resources/fix-anypoint-env.sh
+++ resources/fix-anypoint-env.sh
@@ -2,7 +2,7 @@
 # Fix lowercase env name for DOPS-219
 
 
-env_file=$(ls src/main/resources/|grep .yaml$ |grep 'dev')
+env_file=$(ls src/main/resources/|grep 'dev')
 if [ ! -z "$env_file" ]; then
     new_env_file=$(echo "env_file" | sed -e "s/dev/\Dev/g")
     cp src/main/resources/$env_file src/main/resources/$new_env_file
