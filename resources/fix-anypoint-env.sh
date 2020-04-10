0a946fef7785dd0e1ec460830fee2e3d2779aab5
Serghey Rodin <srodin@ms3-inc.com>
2020-04-10 14:51:31 +0300

    added fix for Prod env

--- resources/fix-anypoint-env.sh
+++ resources/fix-anypoint-env.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 # Fix lowercase env name for DOPS-219
 
-echo "Before"
-env_file=$(ls src/main/resources/|grep 'dev')
-if [ ! -z "$env_file" ]; then
-    new_env_file=$(echo "$env_file" | sed -e "s/dev/\Dev/g")
-    cp src/main/resources/$env_file src/main/resources/$new_env_file
+dev_old=$(ls src/main/resources/|grep dev)
+dev_new=$(echo "$dev_old" | sed 's/dev/Dev/')
+if [ ! -z "$dev_old" ]; then
+    cp src/main/resources/$dev_old src/main/resources/$dev_new
 fi
+
+prod_old=$(ls src/main/resources/|grep prod)
+prod_new=$(echo "$prod_old" | sed 's/prod/Production/')
+if [ ! -z "$prod_old" ]; then
+    cp src/main/resources/$prod_old src/main/resources/$prod_new
+fi
+
 ls src/main/resources
