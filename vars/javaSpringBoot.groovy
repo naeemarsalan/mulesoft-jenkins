7828ad0aea26d0f186fbd9435797adc3eb2856e5
Fiodor Andruhin <fandruhin@ms3-inc.com>
2020-04-30 12:09:27 +0300

    debugging deployment stage #4

--- vars/javaSpringBoot.groovy
+++ vars/javaSpringBoot.groovy
@@ -10,6 +10,7 @@ def call(Map pipelineParams) {
       namespace = "java-springboot-apps" // without "-dev/-prod" 
     }
     stages {
+/*  
       stage('Test') {
         steps {
           container('maven') {
@@ -94,28 +95,44 @@ def call(Map pipelineParams) {
           }
         }
       }
-
+*/
       stage('Deploy to k8s') {
         when {
           expression { GIT_BRANCH ==~ /(.*master|.*develop)/ }
         }
         steps {
           container('kubectl') {
+            
+            // Debug
+            script {
+              pom = readMavenPom file: 'pom.xml'
+              artifactName = readMavenPom().getArtifactId()
+              version = readMavenPom().getVersion()
+              groupName = readMavenPom().getGroupId()
+              if ( GIT_BRANCH =~ "develop")
+                appEnv = "dev"
+              if ( GIT_BRANCH =~ "master")
+                appEnv = "prod"
+              echo "${appEnv}"
+              if ( appEnv =~ "dev")
+                version = "latest"
+            }
+
             writeFile([file: 'deployment.yaml', text: libraryResource('kube/manifests/javaspringboot/deployment.yaml')])
             writeFile([file: 'istio-vs.yaml', text: libraryResource('kube/manifests/javaspringboot/istioGwSnippet.yaml')])
             writeFile([file: 'istio-gw.yaml', text: libraryResource('kube/manifests/javaspringboot/istioVs.yaml')])
-            sh '''
-              envsubst < deployment.yaml | tee ${appName}-deployment.yaml 1>/dev/null
-              echo "This is our deployment:"
+            sh """
+              cat deployment.yaml
+              envsubst < deployment.yaml > ${appName}-deployment.yaml
               cat ${appName}-deployment.yaml
+            """
+/*
               envsubst < istio-vs.yaml | tee ${appName}-istio-vs.yaml 1>/dev/null
               echo "This is istio VS config:"
               cat ${appName}-istio-vs.yaml
               envsubst < istio-gw.yaml | tee ${appName}-istio-gw.yaml 1>/dev/null
               echo "This snippet should be added to k8s gateway configuration:"
               cat ${appname}-istio-gw.yaml
-            '''
-/*
               kubectl apply -f ${appName}-deployment.yaml
               kubectl delete pods -l app=${appName} -n ${namespace}-${appEnv}
 */
